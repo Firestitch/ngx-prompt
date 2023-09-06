d60516c3c26716af09c51f1d56274149f5e15bf6
Raymond Gigliotti <ray@firestitch.com>
2023-09-06 16:48:45 -0400

    removed fsForm circular reference

--- src/app/components/prompt-autocomplete/fs-prompt-autocomplete.module.ts
+++ src/app/components/prompt-autocomplete/fs-prompt-autocomplete.module.ts
@@ -2,31 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-// Material
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 
 import { FsAutocompleteModule } from '@firestitch/autocomplete';
-import { FsFormModule } from '@firestitch/form';
 import { FsDialogModule } from '@firestitch/dialog';
 
-// Dialog
 import { FsPromptAutocompleteComponent } from './prompt-autocomplete.component';
 
 @NgModule({
   imports: [
-    // Angular
     CommonModule,
     FormsModule,
 
-    // Material
     MatDialogModule,
     MatButtonModule,
 
-    // Firestitch
     FsAutocompleteModule,
     FsDialogModule,
-    FsFormModule,
   ],
   entryComponents: [
     FsPromptAutocompleteComponent,
