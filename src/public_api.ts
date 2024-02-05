d5bfabc1030bf041ae9d1cd26d435eafa925e81a
Mike Paterson <mike@firestitch.com>
2024-02-05 12:24:43 -0500

    interfaces

--- src/public_api.ts
+++ src/public_api.ts
@@ -3,20 +3,25 @@
  */
 
 // Modules
-export { FsPromptModule } from './app/fs-prompt.module';
-export { FsPromptConfirmModule } from './app/components/prompt-confirm/fs-prompt-confirm.module';
-export { FsPromptAutocompleteModule } from './app/components/prompt-autocomplete/fs-prompt-autocomplete.module';
 export { FsPromptAutocompleteChipsModule } from './app/components/prompt-autocomplete-chips/fs-prompt-autocomplete-chips.module';
-export { FsPromptSelectModule } from './app/components/prompt-select/fs-prompt-select.module';
-export { FsPromptInputModule } from './app/components/prompt-input/fs-prompt-input.module';
+export { FsPromptAutocompleteModule } from './app/components/prompt-autocomplete/fs-prompt-autocomplete.module';
+export { FsPromptConfirmModule } from './app/components/prompt-confirm/fs-prompt-confirm.module';
 export { FsPromptDateModule } from './app/components/prompt-date/fs-prompt-date.module';
+export { FsPromptInputModule } from './app/components/prompt-input/fs-prompt-input.module';
+export { FsPromptSelectModule } from './app/components/prompt-select/fs-prompt-select.module';
+export { FsPromptModule } from './app/fs-prompt.module';
 
 // Classes
 export * from './app/classes/prompt-config';
 export { FsPromptConfirmConfig } from './app/classes/prompt-confirm-config';
 
 // Interfaces
-export { IFsPromptButtonConfig, IFsPromptConfig } from './app/interfaces/prompt-config';
+export {
+  IFsPromptAutocompleteChipsConfig,
+  IFsPromptAutocompleteConfig, IFsPromptButtonConfig, IFsPromptConfig, IFsPromptDateConfig,
+  IFsPromptDateTimeConfig,
+  IFsPromptInputConfig, IFsPromptSelectConfig
+} from './app/interfaces/prompt-config';
 export { FsValuesFunction } from './app/interfaces/values-function';
 
 // Services
